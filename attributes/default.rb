b4e0abdb22f9a1be00a774f94e0b292ec3289009
Tim Redding <tim@Timothys-MacBook-Pro.local>
2012-10-10 22:37:09 +0100

    Upgrade ro Grails 2.1.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['grails']['version'] = "2.0.4"
+default['grails']['version'] = "2.1.1"
 default['grails']['archive_name']  = "grails-#{node['grails']['version']}.zip"
 default['grails']['source_url']  = "http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/#{node['grails']['archive_name']}"
 default['grails']['install_path']  = "/usr/local/grails-#{node['grails']['version']}"
